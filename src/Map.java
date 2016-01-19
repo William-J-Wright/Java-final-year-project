80e29f53b51de1f532173a613a435ab906e82168
Will <Will@Will-PC>
2016-01-19 16:32:47 +0000

    working shorest path

--- src/Map.java
+++ src/Map.java
@@ -3,6 +3,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.util.Random;
 import java.awt.event.ActionListener;
+import java.util.Arrays;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -11,13 +12,15 @@ import javax.swing.JFrame;
 public class Map extends JPanel {
 
     final static JFrame frame = new JFrame("SIMULATOR");
+    final static int TRIED = 2;
+    final static int PATH = 3;
     public static int holdj =0;
     public static int holdi =0;
     public static int left =0;
     public static int but = 1;
     public static int move = 1;
-    public static final int NUM_ROWS = 100;
-    public static final int NUM_COLS = 100;
+    public static final int NUM_ROWS = 50;
+    public static final int NUM_COLS = 50;
     public Color black = Color.BLACK;
     public static final int PREFERRED_GRID_SIZE_PIXELS = 10;
         boolean cantMoveUp = false;
@@ -27,46 +30,58 @@ public class Map extends JPanel {
     // which will include things like dimensions, color, properties in the
     // game world.  Keeping simple just to illustrate.
     private final Color[][] worldMap;
-
+    private int[][] map;
+    private int height;
+    private int width;
     public Map(){
         Color gray = Color.GRAY;
         this.worldMap = new Color[NUM_ROWS][NUM_COLS];
+        this.map = new int[NUM_ROWS][NUM_COLS];
         Random r = new Random();
         // Randomize the terrain
         for (int i = 0; i < NUM_ROWS; i++) {
             for (int j = 0; j < NUM_COLS; j++) {
                 double rand = Math.random() * ( 1 - 0 );
-                if(j > 95 & i > 40 & i < 60)
+                if(j > 40 & i > 20 & i < 30)
                 {
-                    if(j == 97 & i == 50)
+                    if(j == 47 & i == 25)
                     {
                         holdj = j;
                         holdi = i;
                         this.worldMap[i][j] = Color.GREEN;
+                        this.map[i][j] = 2;
                     }else{
+                        this.map[i][j] = 1;
                         this.worldMap[i][j] = Color.GRAY;
                     }
                 }
                 else
                 if(rand <0.5)
                 {
-                    if(j >0 & i >10 & i <90){
+                    if(j >0 & i >1 & i <99){
                     if(gray.getRGB() == worldMap[i][j-1].getRGB()){
-                         int makemeGray = 1 + (int)(Math.random() * 7);
+                         int makemeGray = 1 + (int)(Math.random() * 10);
                          if(makemeGray > 3){
                          this.worldMap[i][j] = Color.GRAY;
-                         }else{this.worldMap[i][j] = Color.BLACK;}
+                         this.map[i][j] = 1;
+                         }else{this.worldMap[i][j] = Color.BLACK;
+                         this.map[i][j] = 0;}
                     }
                     else{
                     this.worldMap[i][j] = Color.BLACK;
+                    this.map[i][j] = 0;
                 }}else{
                     this.worldMap[i][j] = Color.BLACK;
+                    this.map[i][j] = 0;
                 }}
                 else {
                     this.worldMap[i][j] = Color.GRAY;
+                    this.map[i][j] = 1;
                 }
             }
         }
+        this.height = 50;
+        this.width = 50;
         int preferredWidth = NUM_COLS * PREFERRED_GRID_SIZE_PIXELS;
         int preferredHeight = NUM_ROWS * PREFERRED_GRID_SIZE_PIXELS;
         setPreferredSize(new Dimension(preferredWidth, preferredHeight));
@@ -76,8 +91,6 @@ public class Map extends JPanel {
     public void paintComponent(Graphics g) {
         // Important to call super class method
         super.paintComponent(g);
-        
-        int counter = 0;
         // Clear the board
         g.clearRect(0, 0, getWidth(), getHeight());
         // Draw the grid
@@ -113,7 +126,6 @@ public class Map extends JPanel {
                 g.fillRect(x, y-10, rectWidth, rectHeight);
                 holdj -=1;
                 move = 0;
-                counter = 0;
                 }else if(left == 1 ){
                     if(worldMap[i-1][j] == black){
                         cantMoveLeft = true;
@@ -167,6 +179,7 @@ public class Map extends JPanel {
             @Override
             public void run() {
                 Map map = new Map();
+               
                 JButton ba = new JButton("MAKE BIG");
                 ba.addActionListener(new ActionListener() {
 
@@ -183,6 +196,9 @@ public class Map extends JPanel {
                 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                 frame.pack();
                 frame.setVisible(true);
+                 boolean solved = map.solve();
+                System.out.println("Solved: " + solved);
+                System.out.println(map.toString());
             }
         Runnable helloRunnable = new Runnable() {
             public void run() {
@@ -192,5 +208,85 @@ public class Map extends JPanel {
             }
         };
         });
+    }
+    public boolean solve() {
+        return traverse(2,2);
+    }
+
+    private boolean traverse(int i, int j) {
+        if (!isValid(i,j)) {
+            return false;
+        }
+
+        if ( isEnd(i, j) ) {
+            map[i][j] = PATH;
+            return true;
+        } else {
+            map[i][j] = TRIED;
+        }
+
+        // North
+        if (traverse(i - 1, j)) {
+            map[i-1][j] = PATH;
+            return true;
+        }
+        // East
+        if (traverse(i, j + 1)) {
+            map[i][j + 1] = PATH;
+            return true;
+        }
+        // South
+        if (traverse(i + 1, j)) {
+            map[i + 1][j] = PATH;
+            return true;
+        }
+        // West
+        if (traverse(i, j - 1)) {
+            map[i][j - 1] = PATH;
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean isEnd(int i, int j) {
+        return i == 25 && j == 25;
+    }
+
+    private boolean isValid(int i, int j) {
+        if (inRange(i, j) && isOpen(i, j) && !isTried(i, j)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean isOpen(int i, int j) {
+        return map[i][j] == 1;
+    }
+
+    private boolean isTried(int i, int j) {
+        return map[i][j] == TRIED;
+    }
+
+    private boolean inRange(int i, int j) {
+        return inHeight(i) && inWidth(j);
+    }
+
+    private boolean inHeight(int i) {
+        return i >= 0 && i < height;
+    }
+
+    private boolean inWidth(int j) {
+        return j >= 0 && j < width;
+    }
+
+    public String toString() {
+        String s = "";
+        for (int[] row : map) {
+            s += Arrays.toString(row) + "\n";
+        }
+
+        return s;
     }
 }
