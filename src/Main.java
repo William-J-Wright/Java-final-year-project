7eb9bf6081e7cdf1984d4e7d708c7a71743669b0
Will <Will@Will-PC>
2016-01-24 17:53:40 +0000

    a lot of changes, implimenting Astar

--- src/Main.java
+++ src/Main.java
@@ -13,9 +13,18 @@ import java.awt.Graphics;
 import java.awt.Point;
 import javax.swing.JPanel;
 import java.awt.*;
+import static java.awt.Color.BLACK;
 import static java.awt.Color.BLUE;
 import static java.awt.Color.CYAN;
+import static java.awt.Color.GRAY;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Arrays;
 import java.util.Random;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 /**
@@ -25,22 +34,20 @@ import javax.swing.SwingUtilities;
 public class Main extends JPanel{
     public static int[][] map;
     final static JFrame frame = new JFrame("SIMULATOR");
-    private Color[][] worldMap;
-    private int height;
-    private int width;
+    public static Color[][] worldMap;
+    private final int height;
+    private final int width;
     public static final int PREFERRED_GRID_SIZE_PIXELS = 10;
     /**
      * @param args the command line arguments
      */
     public Main(){
-        Color gray = Color.GRAY;
-        this.worldMap = new Color[200][200];
-        this.map = new int[200][200];
+        Main.worldMap = new Color[50][50];
         Random r = new Random();
         // Randomize the terrain
-        for (int i = 0; i < 200; i++) {
-            for (int j = 0; j < 200; j++) {
-                if(map[i][j] ==0 ){
+        for (int i = 0; i < 50; i++) {
+            for (int j = 0; j < 50; j++) {
+                if(map[i][j] == 0 ){
                     this.worldMap[i][j] = Color.BLACK;
                 }else{
                     this.worldMap[i][j] = Color.GRAY;
@@ -50,47 +57,46 @@ public class Main extends JPanel{
         }
         this.height = 25;
         this.width = 25;
-        int preferredWidth = 200 * PREFERRED_GRID_SIZE_PIXELS;
-        int preferredHeight = 200 * PREFERRED_GRID_SIZE_PIXELS;
+        int preferredWidth = 50 * PREFERRED_GRID_SIZE_PIXELS;
+        int preferredHeight = 50 * PREFERRED_GRID_SIZE_PIXELS;
         setPreferredSize(new Dimension(preferredWidth, preferredHeight));
     }
     
     @Override
      public void paintComponent(Graphics g) {
-        // Important to call super class method
-        paintComponent(g);
+           super.paintComponent(g);
         // Clear the board
         g.clearRect(0, 0, getWidth(), getHeight());
         // Draw the grid
-        int rectWidth = getWidth() / 200;
-        int rectHeight = getHeight() / 200;
-                    for (int i = 0; i < 200; i++) {
-                        for (int j = 0; j < 200; j++) {
+        int rectWidth = getWidth() / 50;
+        int rectHeight = getHeight() / 50;
+                    for (int i = 0; i < 50; i++) {
+                        for (int j = 0; j < 50; j++) {
                         // Upper left corner of this terrain rect
                             int x = i * rectWidth;
                             int y = j * rectHeight;
                             Color terrainColor = worldMap[i][j];
-                            if(map[i][j] == 2)
+                            double ran = Math.random() * ( 1 - 0 );
+                            if(map[i][j] >= 0.5)
                             {
-                                g.setColor(BLUE);
-                            }else if(map[i][j] == 3)
+                                g.setColor(BLACK);
+                            }else if(map[i][j] == 1)
                             {
-                                g.setColor(CYAN);
+                                g.setColor(GRAY);
                             }
                             else{
-                            g.setColor(terrainColor);
+                            g.setColor(GRAY);
                             }
                             g.fillRect(x, y, rectWidth, rectHeight);
                         }
-                    }
-                
+                        }
 }
     public static void main(String[] args) {
         Random rand = new Random();
         //int x = Math.abs(rand.nextInt()%2500);
        // int y = Math.abs(rand.nextInt()%1000);
-        int x = 200;
-        int y = 200;
+        int x = 50;
+        int y = 50;
         System.out.println("Size: ("+x+","+y+")");
         //testing A* search
         map = new int[x][y];
@@ -112,7 +118,6 @@ public class Main extends JPanel{
         map[2][4] = 1;
         map[1][2] = 1;
         
-    
         //why is the plane backwards?
         /*for(int i = 0; i < x; i++){
             for(int j = 0; j<y; j++)
@@ -140,21 +145,49 @@ public class Main extends JPanel{
             if(map[(int)temp.getX()][(int)temp.getY()] != 3 && map[(int)temp.getX()][(int)temp.getY()] != 4)
                 map[(int)temp.getX()][(int)temp.getY()] = 8;
         }
-                Main Main = new Main();
-                frame.add(Main);
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                frame.pack();
-                frame.setVisible(true);
-        
+       
         //reprent map
         /*for(int i = 0; i < x; i++){
             for(int j = 0; j<y; j++)
                 System.out.print(map[i][j] + " ");
             System.out.println();
         }*/
-        path.echoAllPoints();
-       
-    }
-    
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                Main main = new Main();
+               
+                JButton ba = new JButton("MAKE BIG");
+                ba.addActionListener(new ActionListener() {
 
+                    @Override
+                    public void actionPerformed(ActionEvent ae) {
+                    ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+                    executor.scheduleAtFixedRate(helloRunnable, 0, 500, TimeUnit.MILLISECONDS);
+                    }
+                });
+                
+                frame.add(ba);
+                ba.setSize(50,50);
+                frame.add(main);
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.pack();
+                frame.setVisible(true);
+                Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+                frame.setLocation(dim.width/2-frame.getSize().width/2, dim.height/2-frame.getSize().height/2);
+                 
+                
+            }
+        Runnable helloRunnable = new Runnable() {
+            public void run() {
+                frame.repaint();
+                String s = "";
+        for (int[] row : map) {
+            s += Arrays.toString(row) + "\n";
+        }
+                System.out.println(s);
+            }
+        };
+        });
+    }
 }
